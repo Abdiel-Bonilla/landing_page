d0b504f94cd9302caa35e8fa5693f866929b2e52
bran-can19 <Candanedabrandon@gmail.com>
2026-06-09 13:06:39 -0600

    feat home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { Home } from "./Components/Home";
+
 export default function App() {
   return (
-    <h1 className="text-2xl w-full text-center font-bold">Welcome to My landing page ✌️</h1>
-  )
-  
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to My landing page ✌️
+      </h1>
+      <hr />
+      <Home />
+    </>
+  );
 }
